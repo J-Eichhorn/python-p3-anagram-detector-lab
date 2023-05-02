6780afcf222e276774991c716698a3f8f373d2d9
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-05-02 17:34:11 -0400

    passes tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word_letters = sorted([letter for letter in word])
+
+    def match(self, list):
+        word_list = []
+        for word in list:
+            if sorted([letter for letter in word]) == self.word_letters:
+                word_list.append(word)
+        return word_list
